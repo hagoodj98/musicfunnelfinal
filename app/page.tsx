1a18f5b30fb1aba5ca5de30979056eb9c9902efe
Jaiquez Hagood <west00brook@gmail.com>
2024-12-26 16:53:55 -0500

    added videos

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Form from './components/SubscriptionForm';
 export default function Home() {
   return (
     <div className="">
-      <h1>Squeeze</h1>
+      <h1 className=''>I'M GIVING AWAY 4 OF MY MOST POPULAR PRODUCTS TO THE FIRST 50 FANS, ABSOLUTELY FREE!</h1>
+      <h3>Plus Get Early Access To New Music, Livestreams, And Exclusive Merch Giveaways</h3>
       <Form />
     </div>
   );
