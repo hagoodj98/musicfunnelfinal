950f8d24ff6592ba27a7645688d1a8ee95c25cdc
Jaiquez Hagood <west00brook@gmail.com>
2025-04-17 10:48:18 -0400

    made some adjustments

--- app/components/LandingToastNotifier.jsx
+++ app/components/LandingToastNotifier.jsx
@@ -1,21 +1,22 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useSearchParams } from 'next/navigation';
+
+let hasToastShown = false;
 
 const LandingToastNotifier = () => {
   const searchParams = useSearchParams();
   const hasShown = useRef(false);
   const msg = searchParams.get('msg');
 
-
   useEffect(() => {
-    const msg = searchParams.get('msg');
+   
     if (msg && !hasShown.current) {
       hasShown.current = true;
       toast.info(decodeURIComponent(msg));
-        // Remove the query parameter so the toast doesn't reappear
+      // Remove the query parameter so the toast doesn't reappear
       window.history.replaceState({}, document.title, window.location.pathname);
     }
   }, [msg]);
