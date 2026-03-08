ada8760c3fd4b664c10a3db5d3df0bc4c1323af2
Jaiquez Hagood <west00brook@gmail.com>
2026-03-08 16:59:12 -0400

    testing

--- app/utils/mailchimp.js
+++ app/utils/mailchimp.js
@@ -1,9 +1,9 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
-
+import mailchimp from "@mailchimp/mailchimp_marketing";
+//
 mailchimp.setConfig({
-    apiKey: process.env.MAILCHIMP_API_KEY,
-    server: process.env.MAILCHIMP_SERVER_PREFIX,
-  }); 
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER_PREFIX,
+});
 
 // Export the configured Mailchimp client
 export const mailchimpClient = mailchimp;
